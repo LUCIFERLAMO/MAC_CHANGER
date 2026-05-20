77225526745e8423b473db1d435a596ec01eab5e
RITHIK SHEKAR C <rithikbravogmail.com>
2026-05-20 23:41:42 +0530

    changed the subprocess call from string to list to prevent "input hijacking"

--- mac_changer.py
+++ mac_changer.py
@@ -6,13 +6,23 @@ print("-" * 60)
 
 
 interface = input("Enter the interface (eth0): ")
+if interface != "eth0":
+    print("Invalid choose from the given option")
+    exit(0)
+      
 new_mac = input("Enter a mac address (12 numbers/characters): ")
 print()
 print(f"[+] changing MAC address for {interface} to {new_mac}")
 
-subprocess.call(f"ifconfig {interface} down", shell=True)
-subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
-subprocess.call(f"ifconfig {interface} up", shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
 
 print("[+] DONE!")
+print("[+] check the new MAC address")
+print()
+subprocess.call(["ifconfig"])
+print()
+print("[+] Thank you")
+
 print("-" * 60)
