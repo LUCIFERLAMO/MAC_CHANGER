eb4f9282d9c231a555bc60f72c1efeb31efc4530
RITHIK SHEKAR C <rithikbravogmail.com>
2026-05-24 02:37:30 +0530

    made a function and it looks neat now

--- mac_changer.py
+++ mac_changer.py
@@ -3,30 +3,29 @@
 import subprocess 
 import optparse
 
+def change_mac_address(interface, mac):
+    print("-" * 60)
+    print()
+    print(f"[+] changing MAC address for {interface} to {mac}")
+    
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", mac])
+    subprocess.call(["ifconfig", interface, "up"])
+    
+    print("[+] DONE!")
+    print("[+] check the new MAC address")
+    print()
+    subprocess.call(["ifconfig", interface])
+    print()
+    print("[+] Thank you")
+    print("-" * 60)
+
 parse = optparse.OptionParser()
 
 parse.add_option("-i", "--Interface", dest="INTERFACE", help="Enter the Interface name to change it")
 parse.add_option("-m", "--mac", dest="MAC_ADDRESS", help="Enter your custom mac address")
+
 (options, arguments) = parse.parse_args()
 
-new_mac = options.MAC_ADDRESS
-interface = options.INTERFACE
-print("-" * 60)
-
-      
-
-print()
-print(f"[+] changing MAC address for {interface} to {new_mac}")
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
-
-print("[+] DONE!")
-print("[+] check the new MAC address")
-print()
-subprocess.call(["ifconfig", interface])
-print()
-print("[+] Thank you")
-
-print("-" * 60)
+change_mac_address(options.INTERFACE, options.MAC_ADDRESS)
+    
